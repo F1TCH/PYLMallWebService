143802e8443ffb3e10ced86dcf49fb640abcf86c
dixusq <idil.boyka@gmail.com>
2014-11-26 18:34:26 +0800

    basic classes and repositories
    got the basic classes..some parts havent code yet

--- EWDTWebServiceApp/EWDTWebServiceApp/Controllers/BidController.cs
+++ EWDTWebServiceApp/EWDTWebServiceApp/Controllers/BidController.cs
@@ -4,10 +4,25 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using EWDTWebServiceApp.Models;
+using EWDTWebServiceApp.Repositories;
 
 namespace EWDTWebServiceApp.Controllers
-{
+
     public class BidController : ApiController
     {
+        static readonly IBidRepository repository = new BidController();
+
+        public IEnumerable<BidClass> GetAllBid()
+        {
+            BidClass bid = repository.GetById(Id);
+            if (item == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            return item;
+        }
+        public HttpResponseMessage PostBid(BidClass item)
+
     }
-}
+
